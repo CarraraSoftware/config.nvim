175c5f9b8c2805138a7211761e4d6e44cd34b878
Carrara Software <softwarecarrara@gmail.com>
2026-05-01 12:45:14 -0300

    add callback for treesitter filetype autocmd

--- init.lua
+++ init.lua
@@ -278,6 +278,18 @@ treesitter.setup({
 treesitter.install({
     "lua", "python", "go", "html", "json", "xml", "sql", "templ", "javascript",
 })
+vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+        local lang = vim.treesitter.language.get_lang(args.match)
+        if vim.list_contains(treesitter.get_available(), lang) then
+            if not vim.list_contains(treesitter.get_installed(), lang) then
+                treesitter.install(lang):wait()
+            end
+            vim.treesitter.start(args.buf)
+        end
+    end,
+    desc = "Enable nvim-treesitter and install parser if not installed"
+})
 ---------
 
 -- TELESCOPE --
